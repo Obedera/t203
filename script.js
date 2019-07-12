9f883e6c45152fdb7d7d02d2629b0f28713ce13b
Obede <obede.silva7@gmail.com>
2019-07-12 15:25:59 -0300

    Correção função aleatorio

--- script.js
+++ script.js
@@ -24,5 +24,5 @@ function parOuImpar(a){
 
 const aleatorio = function (min, max){
     // return Math.floor(Math.random()*100); de 0 a 100
-    return Math.floor(Math.random()*(max-min))+min;
+    return Math.floor(Math.random()*(max-min+1))+min;
 }
