863e7ed0e8a11c636b19fe3ec0077517ba4d4d12
Obede <obede.silva7@gmail.com>
2019-07-13 21:38:04 -0300

    Correção exercicio 2

--- script2.js
+++ script2.js
@@ -1,40 +1,26 @@
-// let idade = Number(prompt('Digite sua Idade'));
-// let qtdEmprestimo = Number(prompt('Digite o valor desejado de emprestimo'));
-// let salario = Number(prompt('Digite seu salario'));
-
 function avaliarEmprestimo(idade, qtdEmprestimo, salario, parcelas){
-
     if((idade>=22 ) && (idade<=55) && (qtdEmprestimo >= 1000) && (qtdEmprestimo < salario * 15)){
-        console.log('Deu certo') 
+        
         if ((parcelas >= 3 ) && (parcelas <= 20)){
-            // let montante;
-            // montante=qtdEmprestimo*((1+0.08)**parcelas);
-            // montante = montante.toFixed(2);
-            // let par = montante / parcelas;
-            // par = par.toFixed(2);
-            // console.log(`O valor total do empréstimo é de ${montante} e o valor da parcela é de ${parcelas}`)
-            let par = qtdEmprestimo/parcelas;
-            let juros;
-            for(let i=1; i<parcelas;i++){
-                juros = (1+(0.08*par))**i;
-                console.log(par+juros);
-                // let rec = (par+(0.08*par));
-                // console.log(par);
-
-                // rec += rec;
+            let vpar = qtdEmprestimo/parcelas;
+            let juros = vpar*(0.01);
+            let vemprestimo = 0;
+            console.log('Emprestimo aprovado');
+            for(i=1;i<=parcelas;i++){
+                vpar = juros+vpar;
+                console.log('O valor da parcela '+i+' é '+vpar.toFixed(2)+'R$');
+                vemprestimo = vemprestimo+vpar;
+                juros = vpar*(0.01);
             }
+            vemprestimo = vemprestimo.toFixed(2);
+            return `O valor total a ser pago é ${vemprestimo}R$`;
         }
-
-        else if (idade < 22)   {
+        else{
             return("numero de parcelas inválidas")
         }
-        
-        
     }
-    
-    else if (idade < 22)   {
+    else{
 
-        return("idade inválida")
+        return("Emprestimo Negado")
     }
-        
 }
